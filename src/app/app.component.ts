2a18ecb6d035c19f18e51ded9a6def7f654c11c3
Santiago Fernandez <santiago.fernandez.pensa@gmail.com>
2025-07-20 17:47:25 -0300

    ready form-new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';
+import { HeaderComponent } from './features/header/header.component';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { NewsFormModalComponent } from '../../shared/news-form-modal/news-form-modal.component';
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonModule],
+  imports: [ButtonModule, NewsFormModalComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  visible = false;
+
+  constructor(private messageService: MessageService) {}
+
+  openModal() {
+    this.visible = true;
+  }
+
+  onNewsCreated() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Noticia creada',
+      detail: 'La noticia fue publicada correctamente',
+    });
+  }
+
+  onModalClosed() {
+    this.visible = false;
+  }
+}
